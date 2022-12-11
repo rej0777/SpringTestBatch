05a826871881d180e2d69d9c07768a1e9b67354f
rej <Rayearth777@wp.pl>
2022-12-11 21:12:22 +0100

    projekt start

---
+++ src/main/java/com/springTestBatch/SpringTestBatchApplication.java
@@ -0,0 +1,13 @@
+package com.springTestBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springTestBatch/SpringTestBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springTestBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
